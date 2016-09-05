c4fb1b1522d1a0cd42368f6b729f7c4c56851d6c
bikingeskimo <roger.schei.bergheim@live.no>
2016-09-05 15:08:17 +0200

    Oppgave 2

---
+++ openssl-example-master/examples.cpp
@@ -0,0 +1,31 @@
+#include "crypto.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "SHA-1 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::sha1("Test")) << endl << endl;
+  
+  cout << "SHA-1 with two iterations" << endl;
+  cout << Crypto::hex(Crypto::sha1(Crypto::sha1("Test"))) << endl;
+  cout << Crypto::hex(Crypto::sha1("Test", 2)) << endl << endl;
+
+  cout << "The derived key from the PBKDF2 algorithm" << endl;
+  cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl << endl;
+  
+  cout << "SHA-256 with one iteration" << endl;
+  cout << Crypto::hex(Crypto::sha256("Test")) << endl << endl;
+  
+  cout << "SHA-256 with two iterations" << endl;
+  cout << Crypto::hex(Crypto::sha256(Crypto::sha256("Test"))) << endl;
+  cout << Crypto::hex(Crypto::sha256("Test", 2)) << endl << endl;
+  
+  cout << "SHA-512 with one iteration" << endl;
+  cout << Crypto::hex(Crypto::sha512("Test")) << endl << endl;
+  
+  cout << "SHA-512 with two iterations" << endl;
+  cout << Crypto::hex(Crypto::sha512(Crypto::sha512("Test"))) << endl;
+  cout << Crypto::hex(Crypto::sha512("Test", 2)) << endl << endl;
+  
+}
